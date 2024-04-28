b5220e8f1886f34b0ff14e85477ec0751fbe80fd
LorenzoMinjares22 <lminjares3@miners.utep,edu>
2024-04-28 15:13:15 -0600

    added buttons but still no implementation

--- project/project/main.c
+++ project/project/main.c
@@ -6,110 +6,132 @@
 
 #define SW1 BIT3/* switch1 is p1.3 */
 
-#define SWITCHES SW1/* only 1 switch on this board */
+#define SW2 BIT0
+#define SW3 BIT1
+#define SW4 BIT2
+#define SW5 BIT3
+
+#define SWITCH_P1 SW1/* button on msp430 board */
 
 
+/*buttons on display mod*/
+
+#define SWITCH_2 SW2
+
+#define SWITCH_3 SW3
+
+#define SWITCH_4 SW4
+
+#define SWITCH_5 SW5
+
+
+
+#define SWITCH_P2 (SWITCH_2 | SWITCH_3 | SWITCH_4 | SWITCH_5)
 int main(void) {
   P1DIR |= LEDS;
   P1OUT &= ~LED_GREEN;
   P1OUT |= LED_RED;
+
+
   
   configureClocks();/* setup master oscillator, CPU & peripheral clocks */
 
-  P1REN |= SWITCHES;/* enables resistors for switches */
-
-  P1IE |= SWITCHES;/* enable interrupts from switches */
-
-  P1OUT |= SWITCHES;/* pull-ups for switches */
-
-  P1DIR &= ~SWITCHES;/* set switches' bits for input */
   
   enableWDTInterrupts();/* enable periodic interrupt */
 
+
+
+  P1REN |= SWITCH_P1;
+
+  P1IE |= SWITCH_P1;
+
+  P1OUT |= SWITCH_P1;
+
+  P1DIR &= ~SWITCH_P1;
+
+
+
+
+
+  P2REN |= SWITCH_P2;
+
+  P2IE |= SWITCH_P2;
+
+  P2IES |= SWITCH_P2;
+
+  P2OUT |= SWITCH_P2;
+  
   or_sr(0x18);		/* CPU off, GIE on */
 }
 
-// global state var to count time
-vwitch_interrupt_handler()
+/* red board side swich*/
+void switch_interrupt_handler_P1() {
 
-{
-
-  char p1val = P1IN;/* switch is in P1 */
+  char p1val = P1IN;
 
 
 
-  /* update switch interrupt sense to detect changes from current buttons */
+  P1IES |= (p1val & SWITCH_P1);
 
-  P1IES |= (p1val & SWITCHES);/* if switch up, sense down */
-
-  P1IES &= (p1val | ~SWITCHES);/* if switch down, sense up */
-
+  P1IES &= (p1val | ~SWITCH_P1);
+}
 
 
-  /* up=red, down=green */
 
-  if (p1val & SW1) {
+void __interrupt_vec(PORT1_VECTOR) Port_1() {
 
-    P1OUT |= LED_RED;
+  if(P1IFG & SWITCH_P1) {
 
-    P1OUT &= ~LED_GREEN;
-
-  } else {
-
+    P1IFG &= ~SWITCH_P1;
     P1OUT |= LED_GREEN;
-
-    P1OUT &= ~LED_RED;
+    // switch_interrupt_handler_P1();
 
   }
 
 }
 
-int secondCount = 0;
-int firstCount = 0;
-int blinkLimit =5;
-int thirdCount = 0;
+
+void __interrupt_vec(PORT2_VECTOR) Port_2() {
+
+  if (P2IFG & SWITCH_2) {
+
+    P2IFG &= ~SWITCH_2;
+
+    
+
+  }
+
+  if (P2IFG & SWITCH_3) {
+
+    P2IFG &= ~SWITCH_3;
+
+    
+
+  }
+
+  if (P2IFG & SWITCH_4) {
+
+    P2IFG &= ~SWITCH_4;
+
+    
+
+  }
+
+  if(P2IFG & SWITCH_5) {
+
+    P2IFG &= ~SWITCH_5;
+
+   
+
+ 
+
+  }
+
+}
 void
 
 __interrupt_vec(WDT_VECTOR) WDT()/* 250 interrupts/sec */
 
 {
-  firstCount++;
- 
-
-  if (firstCount >= 250 && firstCount < 1250) { /* once each sec... */
-    secondCount++;   
-    //secondCount = 0;/* reset count */
-    P1OUT &= ~LED_GREEN;
-    P1OUT |= LED_RED;/* toggle green LED */
-
-    if(secondCount >= blinkLimit){
-      secondCount = 0;
-      P1OUT |= LED_RED;
-    }else{
-      P1OUT &= ~LED_RED;
-    }
-  }
-  if (firstCount >= 1250){
-
-    firstCount = 0;
-    P1OUT &= ~LED_RED;
-    P1OUT ^= LED_GREEN;   //Toogle red every 500 interupts/sec
-
-      
-  }
-  thirdCount ++;
-
-  if (thirdCount >= 250) {  // once each second
-
-    thirdCount = 0;
-
-    blinkLimit ++;     // reduce duty cycle
-
-    if (blinkLimit >= 8)     // but don't let duty cycle go below 1/7.
-
-      blinkLimit = 0;
-
-  }
-
-
+  
 } 
